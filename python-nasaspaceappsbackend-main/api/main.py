c01675c6d50d2f5aedc66e3d0d4558cc6f89a579
Mretmna <mertozcan1006@gmail.com>
2025-10-05 23:51:59 +0300

    Update main.py

--- python-nasaspaceappsbackend-main/api/main.py
+++ python-nasaspaceappsbackend-main/api/main.py
@@ -72,3 +72,11 @@ def perform_calculations():
         "aqi_data": {"AQI": max_value, "Alert": alert},
         "count": len(coords_db),
     }
+    
+    if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",   
+        host="0.0.0.0",
+        port=8000,
+        reload=True   
+    )
